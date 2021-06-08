2b57691ea4eaf9e659b9d3d25825b9897ef24982
Aman Thakur <uttamthakur2999@gmail.com>
2021-06-08 09:49:12 +0530

    Disco Grid

--- block-BJaael/code/script.js
+++ block-BJaael/code/script.js
@@ -1,11 +1,9 @@
 
 let h1 =document.querySelector("h1");
 let allbox =document.querySelector(".allbox");
-// let singleBox = document.querySelector('.box')
 
-
-function randomNumber(){
-    var num = Math.floor(Math.random() * 10)
+function randomNumber(max){
+    var num = Math.floor(Math.random() * max)
     // console.log(num);
     return num;
     
@@ -16,6 +14,7 @@ function getRandomColor() {
     for (var i = 0; i < 6; i++) {
         let randomNumber = Math.floor(Math.random() * 16)
         color = color + letters[randomNumber]
+        
     }
     return color;
     
@@ -24,30 +23,25 @@ function getRandomColor() {
 for (let i=1; i <= 500; i++){
     var singleBox = document.createElement("div");
     singleBox.classList.add("box");
-    singleBox.innerText =randomNumber();
-    singleBox.style.backgroundColor = colorBox();
+    let h3 =document.createElement("h1");
+    let randomNo = randomNumber(500);
+    h3.innerText =randomNo;
+    singleBox.append(h3);
     allbox.append(singleBox);
     
 }
+let boxs = document.querySelectorAll(".box")
 function colorBox(){
      
-    // let boxsColor = getRandomColor();
-    // console.log(boxsColor);
-
-    // return boxsColor;
-    // let boxs = document.querySelectorAll(".box")
-    // boxs.forEach((elm)=>{
-    //     var boxsColor = getRandomColor();
+    boxs.forEach((elm)=>{
+        elm.style.backgroundColor = getRandomColor();
         
-    //    boxsColor = boxsColor.elm;
-     //    console.log(boxsColor);
-    //    return boxsColor;
+        elm.children[0].innerText=randomNumber(500);
 
-    // })
+    });
     
 }
- 
- 
- singleBox.addEventListener('mousemove', colorBox)
- singleBox.addEventListener('mousemove', randomNumber)
+
+ allbox.addEventListener('mousemove', colorBox);
+
  
