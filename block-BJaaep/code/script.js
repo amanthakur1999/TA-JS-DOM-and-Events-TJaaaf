e55801b8d385e1e837e3ef08bbae2351da68fbf4
Aman Thakur <uttamthakur2999@gmail.com>
2021-06-08 19:51:43 +0530

    added flip box assignment

--- block-BJaaep/code/script.js
+++ block-BJaaep/code/script.js
@@ -1,21 +1,18 @@
-let numbers = document.querySelectorAll('.number');
-numbers.forEach((elem, index) => {
-    elem.addEventListener('click',(event) => {
-        event.target.innerText = index + 1;
-
-        setTimeout(() => {
+let number =document.querySelectorAll(".number");
+ number.forEach((elem,index)=>{
+     elem.addEventListener("click",(event)=>{
+         event.target.innerText = index + 1;
+         setTimeout(()=>{
             event.target.innerText = ""
-        },2000)
-    });
-});
+         },2000)
+     });
+ });
 
-var secondBox = document.querySelector('.second' );
-secondBox.addEventListener('click', (event) => {
+ let numbers =document.querySelector(".second");
+numbers.addEventListener("click",(event) => {
     let text = event.target.dataset.text;
     event.target.innerText = text;
-    setTimeout(() => {
-        event.target.innerText = "";
-    },5000)
+    setTimeout(()=>{
+        event.target.innerText ="";
+    },2000)
 })
-
-
